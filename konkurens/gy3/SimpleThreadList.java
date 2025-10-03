b013991fc732f9b9746984352228bf175123f9da
tamasferencz12 <tamasferencz25@gmail.com>
2025-10-03 14:33:13 +0200

    other

--- konkurens/gy3/SimpleThreadList.java
+++ konkurens/gy3/SimpleThreadList.java
@@ -1,33 +1,41 @@
 package gy3;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.Queue;
 
 public class SimpleThreadList {
 
     public static void main(String[] args) throws InterruptedException {
-        List<Integer> list = new ArrayList<>();
+        Queue<Integer> queue = new ConcurrentLinkedQueue<>();
 
         Thread oddThread = new Thread(() -> {
             for (int i = 1; i <= 1_000_000; i += 2) {
-                synchronized (list) {
-                    list.add(i);
-                }
+                queue.add(i);
             }
         });
 
         Thread evenThread = new Thread(() -> {
             for (int i = 2; i <= 1_000_000; i += 2) {
-                synchronized (list) {
-                    list.add(i);
-                }
+                queue.add(i);
+            }
+        });
+
+        Thread fiveThread = new Thread(() -> {
+            for (int i = 5; i <= 1_000_000; i += 5) {
+                queue.add(i);
             }
         });
 
         oddThread.start();
         evenThread.start();
+        fiveThread.start();
 
         oddThread.join();
         evenThread.join();
+        fiveThread.join();
+        // Print all numbers in the queue
+        for (Integer num : queue) {
+            System.out.println(num);
+        }
     }
 }
