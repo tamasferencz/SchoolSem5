358be94e1f9b39d10d591cd57de21a3ef4640721
tamasferencz12 <tamasferencz25@gmail.com>
2026-01-08 20:01:22 +0100

    concurent programming modified + README

--- konkurens/mintazh-TicketSale/TicketSaleSimulator.java
+++ konkurens/mintazh-TicketSale/TicketSaleSimulator.java
@@ -1,11 +1,12 @@
 import java.util.List;
 import java.util.Map;
-import java.util.Queue;
+import java.util.concurrent.*;
 
 /**
  * Egy koncertre negyfajta jegy kaphato: az A, B, C es D szektorokba.
  * Mindegyik jegyfajtabol 5 erheto el, osszesen tehat 20 jegy kaphato.
- * Mindegyik vasarlo eseten adott, melyik szektorba tartozo jegyet szeretne venni.
+ * Mindegyik vasarlo eseten adott, melyik szektorba tartozo jegyet szeretne
+ * venni.
  */
 public class TicketSaleSimulator {
     private static final int SLEEP_TIME_MIN = 100;
@@ -18,18 +19,41 @@ public class TicketSaleSimulator {
     private static final int SELLER_COUNT = TICKET_TYPES.size();
     private static final int CUSTOMER_COUNT = SELLER_COUNT * TICKET_COUNT_PER_SELLER;
 
-    private static Map<String, Integer> TICKET_INVENTORY = /* TODO Hozzuk letre a map-et */;
-    private static Queue<String> TICKET_QUEUE = /* TODO Hozzunk letre egy sort 1-es kapacitassal */;
-    // TODO Hozzuk letre a megfelelo meretu ExecutorService-t: minden vasarlo es mindegyik elado szamara legyen egy szalnak hely benne
+    // ConcurrentHashMap a szálbiztos hozzáférés miatt
+    private static Map<String, Integer> TICKET_INVENTORY = new ConcurrentHashMap<>();
+
+    // BlockingQueue 1-es kapacitással a termelő-fogyasztó mintához
+    private static BlockingQueue<String> TICKET_QUEUE = new ArrayBlockingQueue<>(1);
+
+    // ExecutorService a szálak kezelésére
+    private static ExecutorService executor = Executors.newFixedThreadPool(SELLER_COUNT + CUSTOMER_COUNT);
 
     public static void main(String[] args) {
         // Jegykeszlet feltoltese
         TICKET_TYPES.forEach(str -> TICKET_INVENTORY.put(str, TICKET_COUNT_PER_SELLER));
 
-        // TODO Inditsuk el a sellerAction-t minden jegytipusra kulon szalon
-        // TODO Inditsuk el a customerAction-t minden kliensre a megfelelo jegytipussal kulon szalon
-        // TODO - Jegyfajtankent 5 vasarlonak kell lennie
-        // TODO A szimulacionak SHUTDOWN_TIME elteltevel le kell allnia
+        // Seller szálak indítása minden jegytípusra
+        for (String type : TICKET_TYPES) {
+            executor.execute(() -> sellerAction(type));
+        }
+
+        // Customer szálak indítása (típusonként 5 db)
+        int customerIdCounter = 1;
+        for (String type : TICKET_TYPES) {
+            for (int i = 0; i < 5; i++) {
+                int finalId = customerIdCounter++;
+                executor.execute(() -> customerAction(finalId, type));
+            }
+        }
+
+        // A szimulacio futtatasa SHUTDOWN_TIME ideig, majd leallitas
+        try {
+            Thread.sleep(SHUTDOWN_TIME);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            executor.shutdownNow();
+        }
     }
 
     /**
@@ -38,25 +62,64 @@ public class TicketSaleSimulator {
     private static void sellerAction(String ticketType) {
         System.out.printf("Started seller for %s%n", ticketType);
 
-        // TODO Ha a TICKET_INVENTORY-ban az adott jegyfajta meg nem fogyott el:
-        // TODO - A TICKET_INVENTORY-ban csokkenjen eggyel a jegyek szama
-        // TODO - Adjuk a megfelelo fajta jegyet a TICKET_QUEUE-hoz
-        // TODO - Irjuk ki az alabbi szoveget:
-        System.out.println("New ticket available for " + ticketType);
-        // TODO - Varjunk valamennyit SLEEP_TIME_MIN es SLEEP_TIME_MAX kozott a jegy eladasa utan
+        try {
+            // Amíg van készleten az adott jegyből
+            while (TICKET_INVENTORY.getOrDefault(ticketType, 0) > 0) {
+                // Csökkentjük a készletet és betesszük a sorba
+                // (A sorrend fontos: előbb ellenőrzés/csökkentés, aztán sorba rakás,
+                // de a blocking queue miatt a 'put' blokkolhat, így a készletkezelés logikája
+                // itt egyszerűsítve van a feladatleírás sorrendjéhez igazodva).
 
-        // TODO Miutan elfogyott a raktarrol az osszes ilyen tipusu jegy, irjuk ki az alabbit, es fejezzuk be a futast
-        System.out.printf("Tickets for %s sold out%n", ticketType);
+                // Blokkoló művelet: megvárja, amíg lesz hely a sorban (kapacitás = 1)
+                TICKET_QUEUE.put(ticketType);
+
+                // Készlet csökkentése
+                TICKET_INVENTORY.computeIfPresent(ticketType, (key, val) -> val - 1);
+
+                System.out.println("New ticket available for " + ticketType);
+
+                // Véletlenszerű várakozás
+                Thread.sleep(ThreadLocalRandom.current().nextInt(SLEEP_TIME_MIN, SLEEP_TIME_MAX));
+            }
+
+            System.out.printf("Tickets for %s sold out%n", ticketType);
+
+        } catch (InterruptedException e) {
+            // Executor shutdown esetén ez normális
+            Thread.currentThread().interrupt();
+        }
     }
 
     /**
      * Ez a vasarlo mukodesenek megvalositasa.
      */
     private static void customerAction(int customerId, String ticketType) {
-        // TODO Amig a vasarlo nem vett meg jegyet, tegye a kovetkezoket:
-        // TODO - Nezze meg, van-e jegy a TICKET_QUEUE-ban a megfelelo jegytipusbol (peek muvelet)
-        // TODO - Ha van, akkor vegye ki es menjen el a koncertre: irja ki az alabbi szoveget, es fejezze be a futasat
-        System.out.printf("Customer %d got a ticket to %s%n", customerId, ticketType);
-        // TODO - Varjon valamennyit SLEEP_TIME_MIN es SLEEP_TIME_MAX kozott a kovetkezo vizsgalat elott
+        try {
+            boolean bought = false;
+            while (!bought) {
+                // Megnézzük (peek), mi van a sor elején
+                String availableTicket = TICKET_QUEUE.peek();
+
+                // Ha a sor nem üres ÉS a jegy típusa megegyezik azzal, amit keresünk
+                if (ticketType.equals(availableTicket)) {
+                    // Megpróbáljuk kivenni (remove). A remove(Object) true-t ad vissza, ha sikerült
+                    // kivenni.
+                    // Ez azért fontos, mert a peek és a kivétel között egy másik szál elvihette
+                    // volna a jegyet.
+                    if (TICKET_QUEUE.remove(ticketType)) {
+                        System.out.printf("Customer %d got a ticket to %s%n", customerId, ticketType);
+                        bought = true;
+                    }
+                }
+
+                if (!bought) {
+                    // Ha nem sikerült venni, várunk egy kicsit
+                    Thread.sleep(ThreadLocalRandom.current().nextInt(SLEEP_TIME_MIN, SLEEP_TIME_MAX));
+                }
+            }
+        } catch (InterruptedException e) {
+            // Executor shutdown esetén ez normális
+            Thread.currentThread().interrupt();
+        }
     }
 }
